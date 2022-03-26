8301cbafaf0f4a5f930d5f76e92d64e62f2e051f
Adrian Grillo <adriangrillo@USF-Gold-Wifi-226-146-122.laptops.usf.edu>
2022-03-26 18:22:28 -0400

    login page

--- path-planner-client/src/app/app.module.ts
+++ path-planner-client/src/app/app.module.ts
@@ -6,17 +6,27 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MapComponent } from './map/map.component';
 import { PlannerComponent } from './planner/planner.component';
+import { LoginComponent } from './login/login.component';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     MapComponent,
-    PlannerComponent
+    PlannerComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatInputModule,
+    MatButtonModule
+  ],
+  exports: [
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
