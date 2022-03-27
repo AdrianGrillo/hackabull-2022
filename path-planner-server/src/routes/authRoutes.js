00cacea408b56c2058e0be65d5a3daf08b37ec6d
Frankie Vega <vegaluis1220@gmail.com>
2022-03-26 20:38:19 -0400

    cghj

--- path-planner-server/src/routes/authRoutes.js
+++ path-planner-server/src/routes/authRoutes.js
@@ -1,13 +1,48 @@
-// Bare-minimum code needed for express server
 const express = require('express')
+const mongoose = require('mongoose')
+const jwt = require('jsonwebtoken')
+const User = mongoose.model('User')
+ 
+const router = express.Router()
 
-const app = express()
+router.post('/signup', async (req, res) => {
+    const { email, password } = req.body
 
-app.get('/', (req, res) => {
-     res.send('hi')
+    try {
+        // Save new user to db
+        const user = new User({ email, password })
+        await user.save()
+
+        // sign jwt and send along userId as the payload to be checked by middleware
+        const token = jwt.sign({ userId: user._id }, 'MY_SECRET_KEY')
+        res.send({ token })
+    } catch(err) {
+        return res.status(422).send(err.message)
+    }
 })
 
-app.listen(3000, () => {
-    console.log('Listening on port 3000')
+router.post('/signin', async (req, res) => {
+    const { email, password } = req.body
+
+    // Check if email and password were provided by the user.
+    if(!email || !password) {
+        return res.status(422).send({ error: 'Must provide email and password' })
+    }
+
+    // Check if a user with the submitted email exists in our db.
+    const user = await User.findOne({ email })
+    if(!user) {
+        return res.status(422).send({ error: 'Invalid password or email' })
+    }
+
+    // If the password submitted matches the email, resolve the promise and sign the user into their account. 
+    try {
+        await user.comparePassword(password)
+        const token = jwt.sign({ userId: user._id }, 'MY_SECRET_KEY')
+        res.send({ token })
+    } catch {
+        return res.status(422).send({ error: 'Invalid password or email' })
+    }
 })
 
+module.exports = router
